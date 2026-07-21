5e47b291875784b6555d7df30ea3f7f779b5243f
JiunWeiYin <53963801+JiunWeiYin@users.noreply.github.com>
2026-07-21 20:32:01 +0800

    bigger

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -40,7 +40,7 @@ export function Layout({ title, children }: { title: string; children: ReactNode
       <header className="topbar">
         <div className="topbar-brand">
           <Link to="/">
-            <img src={getIconSrc(iconTheme, 'logo')} alt="AkaAka" width={120} height={80} />
+            <img src={getIconSrc(iconTheme, 'logo')} alt="AkaAka" width={200} height={160} />
           </Link>
           <span className="topbar-title">{title}</span>
         </div>
